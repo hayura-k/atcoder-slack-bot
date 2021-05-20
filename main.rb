fc366a6d1e8d4cb2d0dd0191eb919d9ce7e679a8
yuki hara <hara.0509@icloud.com>
2021-05-21 01:22:35 +0900

    リンクタグを取得

--- main.rb
+++ main.rb
@@ -3,5 +3,13 @@ require 'pry'
 
 driver = Selenium::WebDriver.for :chrome
 sleep 1
-driver.navigate.to 'https://kenkoooo.com/atcoder/#/table/'
-driver.find_element(:css, '#react-bs-table-container')
+driver.get('https://kenkoooo.com/atcoder/#/table/')
+els = driver.find_element(:class, 'react-bs-container-body').find_element(:tag_name, 'tbody').find_elements(:tag_name, 'tr')
+
+els[0].find_element(:tag_name, 'td').find_element(:tag_name, 'a').click
+
+# els[0].each do |el|
+#   el.find_element(:tag_name, 'td')
+# end
+# p els[1]
+sleep 5
